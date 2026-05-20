a98faef56c247daa59ea0f6677adafe876b6b3af
danteasc4 <zexovon@gmail.com>
2026-05-20 18:00:57 -0400

    feat: New scope published!!!!
    - also fix build script

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -42,13 +42,18 @@ const shared = {
 await emptyDir("npm");
 for (const pkg of packages) {
 	await build({
-		entryPoints: [`./packages/${pkg.name}/src/mod.ts`],
+		entryPoints: [`./packages/${pkg.name}/mod.ts`],
 		outDir: `./npm/${pkg.name}`,
 		shims: shared.shims,
-		mappings: pkg.name !== "core" ? {
-			name: "@jgmc/core",
-			version:`^${Deno.args[0]}`,
-		} : undefined,
+		mappings:
+			pkg.name !== "core"
+				? {
+						"./packages/core/mod.ts": {
+							name: "@jgmc/core",
+							version: `^${Deno.args[0]}`,
+						},
+					}
+				: undefined,
 		package: {
 			name: `@jgmc/${pkg.name}`,
 			version: Deno.args[0],
@@ -61,46 +66,13 @@ for (const pkg of packages) {
 		rootTestDir: `./packages/${pkg.name}/test`,
 		postBuild() {
 			Deno.copyFileSync("LICENSE", `npm/${pkg.name}/LICENSE`);
-			Deno.copyFileSync(`packages/${pkg.name}/README.md`, `npm/${pkg.name}/README.md`);
-			copySync(`packages/${pkg.name}/assets`, `npm/${pkg.name}/assets`, { overwrite: true });
+			Deno.copyFileSync(
+				`packages/${pkg.name}/README.md`,
+				`npm/${pkg.name}/README.md`,
+			);
+			copySync(`packages/${pkg.name}/assets`, `npm/${pkg.name}/assets`, {
+				overwrite: true,
+			});
 		},
 	});
 }
-
-// await build({
-// 	entryPoints: ["./src/index.ts"],
-// 	outDir: "./npm",
-// 	shims: {
-// 		deno: true,
-// 	},
-// 	package: {
-// 		name: "toomanycharts",
-// 		version: Deno.args[0],
-// 		description:
-// 			"A small, client-side, 0-dependency package for quickly generating simple SVG-based charts.",
-// 		license: "MIT",
-// 		repository: {
-// 			type: "git",
-// 			url: "git+https://github.com/danteasc4/toomanycharts.git",
-// 		},
-// 		bugs: {
-// 			url: "https://github.com/danteasc4/toomanycharts/issues",
-// 		},
-// 		keywords: [
-// 			"charts",
-// 			"graphs",
-// 			"svg",
-// 			"data-visualization",
-// 			"ssr",
-// 			"lightweight",
-// 		]
-// 	},
-// 	compilerOptions: {
-// 		lib: ["ESNext", "DOM"],
-// 	},
-// 	rootTestDir: "./test",
-// 	postBuild() {
-// 		Deno.copyFileSync("LICENSE", "npm/LICENSE");
-// 		Deno.copyFileSync("README.md", "npm/README.md");
-// 	},
-// });
